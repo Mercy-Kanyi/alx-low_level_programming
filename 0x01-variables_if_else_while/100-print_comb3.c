54e844ba2c011cf5083754aacf0082d1fbcf39f3
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-03-15 09:19:38 +0000

    tests

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@
 
 /**
  * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -17,11 +17,11 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-
+			
 			if (digit1 == 8 && digit2 == 9)
 				continue;
 			
@@ -34,3 +34,4 @@ int main(void)
 
 	return (0);
 }
+
