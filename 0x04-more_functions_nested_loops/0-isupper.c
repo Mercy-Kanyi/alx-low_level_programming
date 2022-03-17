6275accd4c5caeeaa73c6eee34998dd12590d812
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-03-17 23:22:55 +0000

    nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,21 @@
-#include "main.h"
-
-/**
- * _isupper - Checks for uppercase characters
- * @c: the character to be checked
- *
- * Return: 1 if c is uppercase, 0 if otherwise
+/*
+ * File: 0-isupper.c
+ * Auth: Brennan D Baraban
  */
 
-int _isupper (int c)
+#include "holberton.h"
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
 }
