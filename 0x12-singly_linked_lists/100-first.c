7617418170203d324e3fd266d86f0abdc813d367
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-04-22 21:13:11 +0000

    review

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void print_sentence(void) __attribute__ ((constructor));
 /**
  * print_sentence - prints a sentence before main
  * Return: nothing
--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -8,7 +8,6 @@ void print_sentence(void);
  */
 int main(void)
 {
-	print_sentence();
     printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
     return (0);
 }
