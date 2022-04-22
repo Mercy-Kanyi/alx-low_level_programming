6f4e0909f96bb254bdfec48c7077ee2ce92f2dd2
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-04-22 17:36:36 +0000

    review

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("Length of string : [%u], string : %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		m++;
