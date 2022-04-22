7fb51e9cf22aeee14dfaa18867c8654f4e927d34
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-04-22 21:08:33 +0000

    review

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
-#include "lists.h"
+#include <stdio.h>
 
 /**
- * print_before_main - Prints a string before the main function is executed
- *
- * Return: Nothing
+ * print_sentence - prints a sentence before main
+ * Return: nothing
  */
 
-void print_before_main(void)
+void print_sentence(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house");
-	printf(" upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
---
+++ 0x12-singly_linked_lists/Tests/100-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_sentence(void);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_sentence();
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
